8228b6eb9a951450c2179e5a61135faa59c4ddb9
Andrii Plygun <andrii.plygun.89@gmail.com>
2026-01-22 11:37:05 +0200

    Added compatibility with Laravel framework v7.x

--- src/Okta.php
+++ src/Okta.php
@@ -28,7 +28,7 @@ use Okta\Users\User;
 class Okta
 {
 
-    const VERSION = '1.2.4';
+    const VERSION = '1.2.5';
 
     public function __construct(Client $client = null, DefaultDataStore $dataStore = null)
     {
